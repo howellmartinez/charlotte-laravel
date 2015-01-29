2bf61b81eda10e2b5288dd0eb5b4f6e46758ff56
howellmartinez <howellmartinez@gmail.com>
2015-01-29 17:39:49 +0800

    no message

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,17 +2,16 @@
 	<head>
 		<title>SJCS Batch Reunion Photo Upload</title>
 		{{ HTML::style('bower_components/dropzone/dist/min/dropzone.min.css') }}	
+		{{ HTML::script('bower_components/dropzone/dist/min/dropzone.min.js') }}
 	</head>
 	<body>
 		<h1>Photo Upload</h1>
 		{{ Form::open(array('url' => '/upload', 'files' => true)) }}
    		<div class="dz-message">
-		<h4>Drag Photos to Upload</h4>
-		<span>Or click to browse</span>
+			<h4>Drag Photos to Upload</h4>
+			<span>Or click to browse</span>
 		</div>
 		{{ Form::close() }}
-
-		{{ HTML::script('bower_components/dropzone/dist/min/dropzone.min.js') }}
 	</body>
 
 </html>
